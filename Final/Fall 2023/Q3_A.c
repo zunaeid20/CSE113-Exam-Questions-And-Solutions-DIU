bc1d3a2e71e279300fb5a13ae7a5abbef55c7cf8
Sk Azmain Zunaeid <skazt2004@gmail.com>
2025-08-25 04:51:33 +0600

    Create Q3_A.c

---
+++ Final/Fall 2023/Q3_A.c
@@ -0,0 +1,16 @@
+//Finding the smallest among 3 numbers of a one dimentional array
+#include<stdio.h>
+int main(){
+    int arr[4];
+    for(int i=0; i<3; i++){
+        scanf("%d", &arr[i]);
+    }
+    int smallest=arr[0];
+    for(int i=0; i<3; i++){
+        if(arr[i]<smallest){
+            smallest=arr[i];
+        }
+    }
+    printf("%d\n", smallest);
+    return 0;
+}
