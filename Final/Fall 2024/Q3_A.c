f00038f9b856dd5ef5d1d0d07aa42159bc579319
Sk Azmain Zunaeid <skazt2004@gmail.com>
2025-08-25 05:15:28 +0600

    Update Q3_A.c

--- Final/Fall 2024/Q3_A.c
+++ Final/Fall 2024/Q3_A.c
@@ -1,1 +1,14 @@
-ABC
+//Finding a substring inside a string
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[1000];
+    printf("Enter the password: ");
+    scanf("%s", str);
+    if (strstr(str, "computerscience") != NULL) { //Look here
+        printf("weak password\n");
+    } else {
+        printf("strong password\n");
+    }
+    return 0;
+}
